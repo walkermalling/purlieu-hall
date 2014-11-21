8a909f12c6a75dd204ecc060a748f901620089dc
Walker Malling <walker.malling@gmail.com>
2014-11-21 10:25:31 -0800

    test accordion style

--- app/js/controllers/home-controller.js
+++ app/js/controllers/home-controller.js
@@ -3,16 +3,28 @@
 
 module.exports = function(app){
   app.controller('homeController', ['$scope', function(){
-    var $ = require('jquery');
-    var menuItems = $('nav .menu-item > a');
 
-    menuItems.on('click', function(){
-      if( $(this).parents('.menu-item').hasClass('active') ){
-        $(this).parents('.menu-item').removeClass('active');
+    var $ = require('jquery');
+    var menuLinks = $('nav .menu-item > a');
+    var menuItems = $('nav .menu-item');
+
+    menuLinks.on('click', function(){
+
+      var $this = $(this);
+
+      if ($this.parents('.menu-item').hasClass('active')){
+        $this.parents('.menu-item')
+          .removeClass('active');
+        menuItems.removeClass('supress');
       } else {
-        $('.menu-item').removeClass('active');
-        $(this).parents('.menu-item').addClass('active');
+        menuItems
+          .removeClass('active')
+            .not($this.parents('.menu-item'))
+            .addClass('supress');
+        $this.parents('.menu-item')
+          .addClass('active');
       }
+
     });
 
   }]);
