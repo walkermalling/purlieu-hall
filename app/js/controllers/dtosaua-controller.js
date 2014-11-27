98f9afc73a1b6bd196312f35bd28edc013f021bd
Walker <walker.malling@gmail.com>
2014-11-27 01:45:59 -0800

    factor header and footer out of views

--- app/js/controllers/dtosaua-controller.js
+++ app/js/controllers/dtosaua-controller.js
@@ -2,14 +2,23 @@
 
 module.exports = function(app){
 
-  app.controller('dtosauaController', ['$cookies', '$location', 
-    function($cookies, $location){
+  app.controller('dtosauaController', ['$cookies', '$location', '$scope',
+    function($cookies, $location, $scope){
 
     if (!$cookies.jwt || $cookies.jwt.length < 10) {
       console.log('not authorized');
       $cookies.jwt = null;
       return $location.path('/');
     }
+
+    $scope.menu = {
+      selection : 'home',
+      select: function(section){
+        $scope.menu.selection = section || 'home';
+      }
+    };
+
+
 
   }]);
 };
