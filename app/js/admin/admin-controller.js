55709415c2bb9679b63b5b774276154ed0d09925
Walker <walker.malling@gmail.com>
2014-12-19 21:34:18 -0800

    add basic section show-hide on admin page

--- app/js/admin/admin-controller.js
+++ app/js/admin/admin-controller.js
@@ -9,6 +9,20 @@ module.exports = function(app){
     // if authorized, set header or redirect
     auth.sendJWT(); 
 
+    $scope.sections = {
+      'cms' : true,
+      'events' : false,
+      'users' : false
+    };
+
+    $scope.goTo = function (selection) {
+      for (var key in $scope.sections) {
+        console.log(key);
+        if (key === selection) $scope.sections[key] = true;
+        else $scope.sections[key] = false;
+      }
+    };
+
 
   }]);
 };
