f45c8fe3b201be464b650baa565607d3acb84596
Walker Malling <walker.malling@gmail.com>
2015-05-16 11:55:28 -0700

    try angular http request

--- app/js/calendar/private-calendar-controller.js
+++ app/js/calendar/private-calendar-controller.js
@@ -1,14 +1,14 @@
-'use strict';
-
 var util = require('util');
 var moment = require('moment');
 var _ = require('lodash');
+var async = require('async');
+var request = require('request');
 
 module.exports = function(app){
 
   app.controller('privateCalendarController', 
-    ['$scope', '$cookies', '$location','calServer',
-    function($scope, $cookies, $location, calServer) {
+    ['$scope', '$cookies', '$location','$http',
+    function($scope, $cookies, $location, $http) {
     
     var publicEvents = [];
     var privateEvents = [];
@@ -18,8 +18,6 @@ module.exports = function(app){
     };
 
     function processCalData (calData) {
-      console.log('processing incoming data:');
-      console.log(calData);
       var entries = [];
       calData.feed.entry.forEach(function (entry) {
         var newEntry = {
@@ -30,56 +28,79 @@ module.exports = function(app){
           var i = item.replace('\n','').split(':');
           var label = i.shift();
           var body = i.join(':').trim();
-
           if (label === 'When') {
             newEntry.start = body.replace('PDT', '').trim();
           } else {
             newEntry[label] = body;
           }
-          
         });
         entries.push(newEntry);
       });
       return entries;
     }
 
-    function weaveCalEvents () {
-      console.log('weaving...');
-      var a = publicEvents;
-      var b = privateEvents;
-      var c = a.concat(b);
-      console.log('result:');
-      console.log(c);
-      var sorted = _.sortBy(c, 'start');
-      $scope.calendars.events = sorted.slice(0, 6).reverse();
+    var publicApi = 'https://www.google.com/calendar/feeds/leidsmes3ofpmmttid821fett0%40group.calendar.google.com/private-71b4e8a5fb8ed17992d6384490aa0b92/basic';
+    var privateApi = 'https://www.google.com/calendar/feeds/9hn5l2l3s0ba2aljtd0m3ndu38%40group.calendar.google.com/private-7c4e5248830b7a12286e7fea810ddaf4/basic';
+    var requests = [
+      function getPublic (callback) {
+        $http({url: publicApi, method: 'GET'})
+          .success(function (data, status, headers, config) {
+            callback(null, processCalData(data));
+        });
+      } ,
+      function getPrivate (callback) {
+        $http({url: privateApi, method: 'GET'})
+          .success(function (data, status, headers, config) {
+            callback(null, processCalData(data));
+        });
+      }
+    ];
+
+    function getCalData () {
+      async.parallel(requests, function munge (arr) {
+        var sorted = _.sortBy(arr[0].concat(arr[1]), 'start');
+        $scope.calendars.events = sorted.slice(0, 6).reverse();
+      });
     }
+   
 
-    $scope.calendars.getSections = function () {
+    // function weaveCalEvents () {
+    //   console.log('weaving...');
+    //   var a = publicEvents;
+    //   var b = privateEvents;
+    //   var c = a.concat(b);
+    //   console.log('result:');
+    //   console.log(c);
+    //   var sorted = _.sortBy(c, 'start');
+    //   $scope.calendars.events = sorted.slice(0, 6).reverse();
+    // }
 
-      calServer.getPublic()
-        .success(function (calEvents) {
-          if (!calEvents || !calEvents.feed) {
-            console.log('error getting public events');
-            console.log(calEvents);
-            return;
-          }
-          publicEvents = processCalData(calEvents);
-          weaveCalEvents();
-        });
+    // $scope.calendars.getSections = function () {
 
-      calServer.getPrivate()
-        .success(function (calEvents) {
-          if (!calEvents || !calEvents.feed) {
-            console.log('error getting private events');
-            return;
-          }
-          privateEvents = processCalData(calEvents);
-          weaveCalEvents();
-        });
+    //   calServer.getPublic()
+    //     .success(function (calEvents) {
+    //       if (!calEvents || !calEvents.feed) {
+    //         console.log('error getting public events');
+    //         console.log(calEvents);
+    //         return;
+    //       }
+    //       publicEvents = processCalData(calEvents);
+    //       weaveCalEvents();
+    //     });
 
-      };
+    //   calServer.getPrivate()
+    //     .success(function (calEvents) {
+    //       if (!calEvents || !calEvents.feed) {
+    //         console.log('error getting private events');
+    //         return;    
+    //       }
+    //       privateEvents = processCalData(calEvents);
+    //       weaveCalEvents();
+    //     });
 
-    $scope.calendars.getSections();
+    //   };
+
+    // $scope.calendars.getSections();
     
   }]);
 };
